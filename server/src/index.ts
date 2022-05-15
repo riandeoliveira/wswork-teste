aa168318abc3f5e356f6a80d84e8d0ff1bea432a
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-15 18:43:13 -0300

    ✨ feat: Create server index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,17 @@
-import express from "express";
+import express, { Express } from "express";
+import { Port } from "./types";
 import cors from "cors";
-import axios from "axios";
+import bodyParser from "body-parser";
+import routes from "./routes/routes";
 
-const app = express();
+const app: Express = express();
+const port: Port = process.env.PORT || 3333;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
 
-app.get("/", async (req, res) => {
-  try {
-    const { data } = await axios.get("https://wswork.com.br/cars.json");
-
-    res.status(200).send(data);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-app.listen(process.env.PORT || 3333, () => {
-  console.log("Server running on http://localhost:3333");
+app.listen(port, (): void => {
+  console.log("Servidor rodando em: http://localhost:3333");
 });
