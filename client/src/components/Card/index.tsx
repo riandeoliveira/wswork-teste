baa8b5a61fa70fe7d79f5fb2e80c5b67d478ce5f
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-16 16:48:34 -0300

    ✨ feat: Create Card component

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,42 +1,29 @@
+import { CardProps } from "../../interfaces";
 import * as S from "./styles";
 
-export const Card = () => {
+export const Card = ({ data }: CardProps): JSX.Element => {
   return (
     <S.Card>
-      <img
-        src="https://d1mcpktd27z5n0.cloudfront.net/cdn/nippokar.com.br/uploads/2020/12/18163346/desk360_corolla_altis-hybrid-premium_branco-perola_070_sprite_11.png"
-        alt=""
-      />
-      <img
-        src="https://i0.wp.com/codex.click/wp-content/uploads/2021/04/03.png?fit=376%2C227&ssl=1"
-        alt=""
-      />
-      <img
-        src="https://cdn.autopapo.com.br/carro/ford/ka-hatch-se-10-flex-2015/destaque-v1.png"
-        alt=""
-      />
-      <img
-        src="https://locaauto.com.br/wp-content/uploads/2017/07/gol-trendline-1-0-mpi-4-portas-completo.png"
-        alt=""
-      />
-      <img
-        src="https://grandecastanhal.com.br/assets/uploads/nt_veiculos/79826-Imagem-ve--culo.png?v=0.2"
-        alt=""
-      />
-      <img
-        src="https://www.nicepng.com/png/full/229-2294741_veculo-fiat-uno-mille-economy-02-portas-bsico.png"
-        alt=""
-      />
-      <img
-        src="https://www.blaisealexanderchevy.com/assets/stock/Expanded/Transparent/640/2008TOY005a_640/2008TOY005a_640_01.png"
-        alt=""
-      />
-      <img src="https://img1.icarros.com/dbimg/imgmodelo/5/479_12.png" alt="" />
-      <img src="https://img1.icarros.com/dbimg/imgmodelo/5/480_14.png" alt="" />
-      <img
-        src="https://www.tragial.com.br/wp-content/uploads/2020/02/ka-placa.png"
-        alt=""
-      />
+      <S.CarHeaderBox>
+        <h2>{data.marca_nome}</h2>
+        <span>{data.ano}</span>
+      </S.CarHeaderBox>
+      <h3>{data.nome_modelo}</h3>
+      <img src={data.imagem} alt="" />
+      <S.CarDataBox>
+        <div>
+          <span>Cor</span>
+          <span>{data.cor}</span>
+        </div>
+        <div>
+          <span>Portas</span>
+          <span>{data.num_portas}</span>
+        </div>
+        <div>
+          <span>Combustível</span>
+          <span>{data.combustivel}</span>
+        </div>
+      </S.CarDataBox>
     </S.Card>
   );
 };
--- client/src/components/Card/styles.ts
+++ client/src/components/Card/styles.ts
@@ -1,8 +1,40 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  background-color: red;
-  width: 250px;
-  height: 250px;
+  width: 300px;
+  color: #6c6c6c;
+  background-color: #151515;
+  height: 400px;
   margin: 20px;
+  padding: 20px;
+  border-radius: 20px;
+
+  h3 {
+    font-size: 32px;
+    color: white;
+  }
+
+  img {
+    margin: 10px 0;
+    width: 100%;
+    height: 180px;
+    object-fit: contain;
+  }
+`;
+
+export const CarHeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  h2 {
+    font-size: 16px;
+  }
+`;
+
+export const CarDataBox = styled.div`
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 0;
+  }
 `;
