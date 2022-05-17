5bf020caabe4d40d0df9f3eccb75c93e685ab963
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-17 00:45:46 -0300

    📦 refactor: Add Brand page content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,12 @@ import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { Brand } from "./pages/Brand";
 import { Footer } from "./components/Footer";
-import { Card } from "./components/Card";
 import { api } from "./services/api";
 import { useEffect, useState } from "react";
 import { Car } from "./interfaces";
-import { Widget } from "./components/Widget";
 
 export const App = () => {
   const [cars, setCars] = useState<Car[]>([]);
-  const [carBrands, setCarBrands] = useState<string[]>([]);
 
   const getCarsData = async (): Promise<void> => {
     try {
@@ -30,29 +27,10 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <>
-        {cars.map(
-          (car: Car, i: number) =>
-            !carBrands.includes(car.marca_nome) &&
-            setCarBrands([...carBrands, car.marca_nome])
-        )}
-        {carBrands.map((carBrand, i) => (
-          <Widget key={i} title={carBrand}>
-            {cars.map(
-              (car: Car, i: number) =>
-                car.marca_nome === carBrand && <Card key={i} data={car} />
-            )}
-          </Widget>
-        ))}
-        {/* {cars.map((car: Car, i: number) => (
-          <Card key={i} data={car} />
-        ))} */}
-      </>
-
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/marcas" element={<Brand />} />
+          <Route path="/marcas" element={<Brand carList={cars} />} />
         </Routes>
       </main>
       <Footer />
--- client/src/pages/Brand/index.tsx
+++ client/src/pages/Brand/index.tsx
@@ -1,3 +1,26 @@
-export const Brand = () => {
-  return <h1>Marcas</h1>;
+import { BrandProps, Car } from "../../interfaces";
+import { useState } from "react";
+import { Widget } from "../../components/Widget";
+import { Card } from "../../components/Card";
+
+export const Brand = ({ carList }: BrandProps): JSX.Element => {
+  const [carBrands, setCarBrands] = useState<string[]>([]);
+
+  return (
+    <>
+      {carList.map(
+        (car: Car) =>
+          !carBrands.includes(car.marca_nome) &&
+          setCarBrands([...carBrands, car.marca_nome])
+      )}
+      {carBrands.map((carBrand: string, i: number) => (
+        <Widget key={i} title={carBrand}>
+          {carList.map(
+            (car: Car, i: number) =>
+              car.marca_nome === carBrand && <Card key={i} carData={car} />
+          )}
+        </Widget>
+      ))}
+    </>
+  );
 };
