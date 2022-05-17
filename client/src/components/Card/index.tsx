58c883910c6a975f0d3339c374c59b53beec681d
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-05-17 00:44:55 -0300

    📦 refactor: Rename Card component prop

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,27 +1,27 @@
 import { CardProps } from "../../interfaces";
 import * as S from "./styles";
 
-export const Card = ({ data }: CardProps): JSX.Element => {
+export const Card = ({ carData }: CardProps): JSX.Element => {
   return (
     <S.Card>
       <S.CarHeaderBox>
-        <h2>{data.marca_nome}</h2>
-        <span>{data.ano}</span>
+        <h2>{carData.marca_nome}</h2>
+        <span>{carData.ano}</span>
       </S.CarHeaderBox>
-      <h3>{data.nome_modelo}</h3>
-      <img src={data.imagem} alt="" />
+      <h3>{carData.nome_modelo}</h3>
+      <img src={carData.imagem} alt="" />
       <S.CarDataBox>
         <div>
           <span>Cor</span>
-          <span>{data.cor}</span>
+          <span>{carData.cor}</span>
         </div>
         <div>
           <span>Portas</span>
-          <span>{data.num_portas}</span>
+          <span>{carData.num_portas}</span>
         </div>
         <div>
           <span>Combustível</span>
-          <span>{data.combustivel}</span>
+          <span>{carData.combustivel}</span>
         </div>
       </S.CarDataBox>
     </S.Card>
